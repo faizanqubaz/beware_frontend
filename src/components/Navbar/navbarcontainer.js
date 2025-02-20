0826b8a071d9110d28a32fa9f50073c9d86477c2
faizanqubaz <faizanquba1@gmail.com>
2025-02-20 19:41:37 +0500

    adminaaaaaaa

---
+++ src/components/Navbar/navbarcontainer.js
@@ -0,0 +1,213 @@
+import React, { useState, useRef } from 'react';
+import './navbarcontainer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import passuA from '../assets/passu2.jpg';
+// Import additional images
+import passuA1 from '../assets/passu1.jpg';
+import passuA2 from '../assets/passu2.jpg';
+import passuA3 from '../assets/passu3.jpg';
+import passuA4 from '../assets/passu4.jpg';
+import passuA5 from '../assets/yunz1.jpg';
+import passuA6 from '../assets/yunz2.jpg';
+import passuA7 from '../assets/yunz3.jpg';
+import passuA8 from '../assets/yunz4.jpg';
+
+import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
+
+const NavbarContainer = () => {
+  const [isDropdownVisible, setIsDropdownVisible] = useState('passu'); // Default dropdown open
+  const [hoveredItem, setHoveredItem] = useState('passu-item1'); // Default item
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const timeoutRef = useRef(null);
+
+  const handleMouseEnter = (dropdownName) => {
+    timeoutRef.current = setTimeout(() => {
+      clearTimeout(timeoutRef.current);
+      setIsDropdownVisible(dropdownName);
+    }, 700);
+  };
+
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setIsDropdownVisible(null);
+      setHoveredItem(null); // Reset hovered item when leaving
+    }, 700);
+  };
+
+  const handleItemHover = (item) => {
+    setHoveredItem(item);
+  };
+
+  const imagesMap = {
+    'passu-item1': [passuA, passuA1, passuA2,passuA3,passuA4], // List of images for item1
+    'passu-item2': [passuA1, passuA2,passuA5], // List of images for item2
+    'batura-item1': [passuA5, passuA7,passuA8], // Replace with real images
+    'batura-item2': [passuA2,passuA4,passuA6], // Replace with real images
+    // Add more mappings as needed
+  };
+
+  return (
+    <div className="navbar_container_main">
+      <div className="navbar_container_main_slider">
+        <div className="navbar_container_main_slider_flex">
+          <div className="navbar_container_main_slider_flex_div1">
+            <img src={logo} alt="Logo" className="navbar_container_main_slider_flex_logo" />
+          </div>
+ {/* Hamburger Button */}
+ <div className="hamburger-menu" onClick={() => setMenuOpen(!menuOpen)}>
+            <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
+          </div>
+          <div className={`navbar_container_main_slider_flex_div2 ${menuOpen ? 'open' : ''}`}>
+            <ul className="navbar_container_main_slider_flex_ul">
+              {/* Home Link */}
+              <Link to="/" className="navbar_container_main_slider_flex_link">
+                <li className="navbar_container_main_slider_flex_ul_active">Home</li>
+              </Link>
+
+              {/* Passu Dropdown */}
+              <li
+                className="dropdown"
+                onMouseEnter={() => handleMouseEnter('passu')}
+                onMouseLeave={handleMouseLeave}
+              >
+                Passu <FontAwesomeIcon icon={faChevronDown} className="dropdown-icon" />
+                {isDropdownVisible === 'passu' && (
+                  <>
+                    <div
+                      className="custom-dropdown"
+                      onMouseEnter={() => handleMouseEnter('passu')}
+                      onMouseLeave={handleMouseLeave}
+                    >
+                      <div className="custom-dropdown_main_content_slider">
+                        <div className="custom-dropdown_list">
+                          <ul>
+                            <li onMouseEnter={() => handleItemHover('passu-item1')}>TupopDan</li>
+                            <li onMouseEnter={() => handleItemHover('passu-item2')}>Avagadar</li>
+                            <li onMouseEnter={() => handleItemHover('passu-item2')}>Passu Glacier Side</li>
+                            <li onMouseEnter={() => handleItemHover('passu-item2')}>Khuramabad</li>
+                            <li onMouseEnter={() => handleItemHover('passu-item2')}>Surnoabod</li>
+                          </ul>
+                        </div>
+                        <div className="custom-dropdown_images">
+                          {imagesMap[hoveredItem]?.map((image, index) => (
+                            <div key={index} className='passu_images_container_flex'>
+                              <img className='passu_img_class' src={image} alt={`Passu Image ${index + 1}`} />
+                              <p>{`Passu ${index + 1}`}</p>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="buffer-area"></div>
+                  </>
+                )}
+              </li>
+
+              {/* Batura Dropdown */}
+              <li
+                className="dropdown"
+                onMouseEnter={() => handleMouseEnter('batura')}
+                onMouseLeave={handleMouseLeave}
+              >
+                Batura <FontAwesomeIcon icon={faChevronDown} className="dropdown-icon" />
+                {isDropdownVisible === 'batura' && (
+                  <>
+                    <div
+                      className="custom-dropdown"
+                      onMouseEnter={() => handleMouseEnter('batura')}
+                      onMouseLeave={handleMouseLeave}
+                    >
+                      <div className="custom-dropdown_main_content_slider">
+                        <div className="custom-dropdown_list">
+                          <ul>
+                            <li onMouseEnter={() => handleItemHover('batura-item1')}>Yunz Valley</li>
+                            <li onMouseEnter={() => handleItemHover('batura-item2')}>Yashpert Valley</li>
+                            <li onMouseEnter={() => handleItemHover('batura-item2')}>Gucesem Valley</li>
+                            <li onMouseEnter={() => handleItemHover('batura-item2')}>YukhGoz Valley</li>
+                            <li onMouseEnter={() => handleItemHover('batura-item2')}>Kukhail Valley</li>
+                          </ul>
+                        </div>
+                        <div className="custom-dropdown_images">
+                          {imagesMap[hoveredItem]?.map((image, index) => (
+                            <div key={index} className='passu_images_container_flex'>
+                              <img className='batura_img_class' src={image} alt={`Batura Image ${index + 1}`} />
+                              <p>{`Batura ${index + 1}`}</p>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="buffer-area"></div>
+                  </>
+                )}
+              </li>
+              <Link to={'/aboutus'} className="navbar_container_main_slider_flex_link">
+                <li>About Us</li>
+              </Link>
+
+{/* 
+              <li>Blog</li> */}
+
+
+
+
+              {/* Media Dropdown */}
+              <li
+                className="dropdown"
+                onMouseEnter={() => handleMouseEnter('media')}
+                onMouseLeave={handleMouseLeave}
+              >
+                Media <FontAwesomeIcon icon={faChevronDown} className="dropdown-icon" />
+                {isDropdownVisible === 'media' && (
+                  <div
+                    className="media-dropdown"
+                    onMouseEnter={() => handleMouseEnter('media')}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    <div className="media">
+                      <div className="media-dropdown_content">
+                        <ul>
+                          <Link to={'/video'} className='navbar_container_contactus_style'>
+                          <li onMouseEnter={() => handleItemHover('media-item1')}>Videos</li>
+                          </Link>
+                         
+                          <Link to={'/gallery'} className='navbar_container_contactus_style'>
+                         
+                          <li onMouseEnter={() => handleItemHover('media-item2')}>Gallery</li>
+
+                          </Link>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="buffer-area"></div>
+                  </div>
+                )}
+              </li>
+              <Link to={'/sponsors'} className='navbar_container_contactus_style'>
+                <li>Sponsors</li>
+              </Link>
+
+              <Link to={'/terms_and_conditions'} className='navbar_container_contactus_style'>
+                <li >Term & Condition</li>
+              </Link>
+
+              <Link className='navbar_container_contactus_style' to={'/contactus'}>
+                <li>Contact Us</li>
+              </Link>
+
+              <Link className='navbar_container_contactus_style' to={'/admin-sigin'}>
+                <li>Admin</li>
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarContainer;
---
+++ src/components/New_Hunt/newhunt.js
@@ -0,0 +1,117 @@
+import './newhunt.css';
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesLeft, faAnglesRight, faStar, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const NewHunt = () => {
+  const [discountData, setDiscountData] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Fetch data from API on component mount
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://beware-seven.vercel.app/api/v2/ibex?hunttype=newhunttype`);
+        setDiscountData(response.data.data);
+      } catch (error) {
+        console.error('Error fetching discount data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % discountData.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? discountData.length - 1 : prevIndex - 1
+    );
+  };
+
+  // If data is not yet loaded, return a loading spinner
+  if (!discountData.length) {
+    return (
+      <div style={{ textAlign: 'center', padding: '50px' }}>
+        <FontAwesomeIcon icon={faSpinner} spin fontSize={'36px'} color='#dbb127' />
+      </div>
+    );
+  }
+
+  // Calculate the number of items to display (up to 3)
+  const totalItems = discountData.length;
+  const itemsToShow = Math.min(3, totalItems);
+  const visibleImages = [];
+
+  for (let i = 0; i < itemsToShow; i++) {
+    visibleImages.push(discountData[(currentIndex + i) % totalItems]);
+  }
+
+  return (
+    <div className="newhunt_main_container">
+      <div className='newhunt_main_container_slider'>
+        <div className='newhunt_main_container_slider_head'>
+          <div className='newhunt_main_container_slider_flex'>
+            <h2 className='newhunt_main_container_slider_heading'>New hunts</h2>
+            <FontAwesomeIcon color='#dbb127' style={{ marginLeft: '10px' }} icon={faAnglesRight} fontSize={'20px'} />
+          </div>
+          <p className='newhunt_main_container_slider_para_head'>All New Hunts</p>
+        </div>
+
+        <div className="newhunt_main__arrow_left" onClick={handlePrev}>
+          <FontAwesomeIcon icon={faAnglesLeft} fontSize={'36px'} />
+        </div>
+
+        <div className='newhunt_main__image_container'>
+          {visibleImages.map((item) => (
+            <Link
+              key={item.id}
+              to={`/newhunt/${item._id}`}
+              state={{ item }}
+              className='newhunt_main__image_container_one'
+            >
+              <img
+                className='newhunt_main__image_container_one_image'
+                src={item.ibexphotos[0].cloudinary_url}
+                alt={item.description}
+              />
+              <p className='newhunt_main__image_container_one_image_paragraph'>{item.description}</p>
+              <div className='newhunt_main__image_container_one_head'>
+                <p style={{ color: '#dbb127' }}>price from</p>
+                <FontAwesomeIcon icon={faStar} color='#dbb127' />
+                <FontAwesomeIcon icon={faStar} color='#dbb127' />
+                <FontAwesomeIcon icon={faStar} color='#dbb127' />
+                <FontAwesomeIcon icon={faStar} color='#dbb127' />
+                <FontAwesomeIcon icon={faStar} color='#dbb127' />
+                <p style={{ color: '#dbb127' }}>(3.6)</p>
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'space-between', width: '71%', color: 'white' }}>
+                <div className='discount_main__image_container_two_head'>
+                  <p style={{ fontSize: '25px' }}>{'$' + item.newPrice}</p>
+                  <p style={{ textDecoration: 'line-through' }}>{'$' + item.priceOld}</p>
+                </div>
+                <p style={{ fontSize: '14px', textTransform: 'capitalize' }}>
+                  {new Date(item.huntdate).toLocaleDateString()}
+                </p>
+              </div>
+            </Link>
+          ))}
+        </div>
+
+        <div className='newhunt_main__arrow_right_hr'>
+          <hr />
+        </div>
+
+        <div className="newhunt_main__arrow_right" onClick={handleNext}>
+          <FontAwesomeIcon icon={faAnglesRight} fontSize={'36px'} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewHunt;
